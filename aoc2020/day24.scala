63a368fc07acd699b69ad3a8c6dd7e8114cd6f92
hyperchessbot <73081047+hyperchessbot@users.noreply.github.com>
2020-12-24 08:11:34 +0100

    Update day24.scala

--- aoc2020/day24.scala
+++ aoc2020/day24.scala
@@ -2,8 +2,19 @@ val aoc2020SnippetsBaseUrl = "https://raw.githubusercontent.com/hyperchessbot/sn
 val input = scala.io.Source.fromURL(s"$aoc2020SnippetsBaseUrl/day24.input.txt").mkString.split("\n")
 case class Vect(x:Int, y:Int){
   def add(v:Vect):Vect = Vect(x + v.x, y + v.y)
+  def mult(s:Int):Vect = Vect(x * s, y * s)
 }
-val grid = scala.collection.mutable.Map[Vect, Boolean]()
+val directions = Map[String, Vect]("ne" -> Vect(1, 1), "se" -> Vect(1, -1), "sw" -> Vect(-1, -1), "nw" -> Vect(-1, 1), "e" -> Vect(2, 0), "w" -> Vect(-2, 0))
+val grid = scala.collection.mutable.Map[Vect, Boolean](Vect(0,0) -> false)
+for(i <- 0 until 10){
+  for((vect, _) <- grid){
+    for((_, dir) <- directions){
+      val v = vect.add(dir)
+      grid.update(v, false)
+    }  
+  }
+}
+
 case class Instruction(initStr:String){
   var buff = initStr
   val steps = scala.collection.mutable.ArrayBuffer[Vect]()
@@ -25,7 +36,7 @@ case class Instruction(initStr:String){
     }
   }
 }
-val directions = Map[String, Vect]("ne" -> Vect(1, 1), "se" -> Vect(1, -1), "sw" -> Vect(-1, -1), "nw" -> Vect(-1, 1), "n" -> Vect(0, 2), "e" -> Vect(2, 0), "s" -> Vect(0, -2), "w" -> Vect(-2, 0))
+
 val instructions = input.map(Instruction(_)).toList
 for(instruction <- instructions){  
   grid.updateWith(instruction.aggr)(_ match {
@@ -33,5 +44,31 @@ for(instruction <- instructions){
     case Some(flipped) => Some(!flipped)
   })
 }
-//println(instructions(0).initStr, instructions(0).steps)
 println(grid.keySet.count(grid(_)))
+def move(grid:scala.collection.mutable.Map[Vect, Boolean]):scala.collection.mutable.Map[Vect, Boolean] = {
+  val newGrid = scala.collection.mutable.Map[Vect, Boolean]()
+  for((vect, flipped) <- grid){
+    var cnt = 0    
+    for((_, ndir) <- directions){
+      val v = vect.add(ndir)      
+      if(grid.contains(v)){
+        if(grid(vect.add(ndir))) cnt += 1
+      }else{
+        newGrid.update(v, false)
+      }
+    }        
+    val newFlipped = if(flipped){
+      if((cnt == 0)||(cnt > 2)) false else flipped
+    }else{
+      if(cnt == 2) true else flipped
+    }
+    newGrid.update(vect, newFlipped)
+  }
+  newGrid
+}
+
+var temp = grid
+for(i <- 0 until 100){    
+  temp = move(temp)
+}
+println(temp.keySet.count(temp(_)))
