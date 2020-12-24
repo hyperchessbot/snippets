147da9a20590e909967088031df11ca3f873ac09
hyperchessbot <73081047+hyperchessbot@users.noreply.github.com>
2020-12-24 13:15:13 +0100

    Update day23.cpp

--- aoc2020/day23.cpp
+++ aoc2020/day23.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
 #include <string.h>
 
-const int RING_SIZE = 9;
+const int MAX_RING_SIZE = 1000000;
 
 char input[] = "389125467";
 
-int ring[RING_SIZE + 1];
+int ring[MAX_RING_SIZE + 1];
 
-int current = input[0] - '0';
+int current;
 
-void print_ring(){
+void print_ring(int upto){
   int temp = 1;
-  for(int i = 0; i < 9; i++){
-    if(temp == current) std::cout << "*";
+  long prod = 1;
+  for(int i = 0; i < upto; i++){
+    if(temp == 1) std::cout << "( ";
+    //if(temp == current) std::cout << "*";
     std::cout << temp << " ";
+    if(temp == 1) std::cout << ") ";
+    prod = prod * temp;
     temp = ring[temp];
   }
-  std::cout << std::endl;
+  if(upto == 3){
+    std::cout << "prod " << prod;
+  }  
+   std::cout << std::endl;
 }
+
+int RING_SIZE;
 
 void move(){
   int n1 = ring[current];
@@ -42,22 +51,39 @@ void move(){
   ring[n3] = old;
 }
 
-int main() {
-  
-  for(int i = 10; i < RING_SIZE; i++){
-    ring[i] = i + 1;
+void solve(int part){
+  current = input[0] - '0';
+
+  RING_SIZE = strlen(input);
+
+  if(part == 2){
+    RING_SIZE = 1000000;
+
+    for(int i = 10; i < RING_SIZE; i++){
+      ring[i] = i + 1;
+    }
   }
 
   for(int i = 1; i < strlen(input); i++){
     ring[input[i - 1] - '0'] = input[i] - '0';
   }
 
-  ring[input[strlen(input) - 1] - '0'] = input[0] - '0';
-  
-  //ring[RING_SIZE] = current;
+  if(part == 1){
+    ring[input[strlen(input) - 1] - '0'] = input[0] - '0';
+  }else{
+    ring[input[strlen(input) - 1] - '0'] = 10;
+    ring[RING_SIZE] = current;
+  }
 
-  for(int i = 0; i < 100; i++){    
+  for(int i = 0; i < (part == 1 ? 100 : 10000000); i++){    
     move();
-    print_ring();
   }  
+
+  print_ring(part == 1 ? 9 : 3);
+}
+
+int main() {
+  solve(1);
+
+  solve(2);
 }
