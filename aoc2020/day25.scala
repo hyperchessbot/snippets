90fbb367ba4508199a38d66fd703fd4ada88f580
hyperchessbot <73081047+hyperchessbot@users.noreply.github.com>
2020-12-25 06:20:08 +0100

    Update day25.scala

--- aoc2020/day25.scala
+++ aoc2020/day25.scala
@@ -1,5 +1,20 @@
+val divisor = 20201227L
+val subjectNumber = 7L
+
+def transform(pubKey:Long):Long = {
+  var current = 1L
+  for(loop <- 1L until 20L){
+    current = current * subjectNumber
+    current = current % divisor
+    if(current == pubKey) return loop
+  }
+  0L
+}
+
 def solve(input:String):Unit = {
-  
+  val pubKeys = input.split("\n").map(_.toLong).toList
+  println(pubKeys, subjectNumber, divisor)
+  println(transform(5764801))
 }
 
 for(inp <- List("example")){  
