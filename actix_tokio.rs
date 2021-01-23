6336c287319f5e90378ae2d9c2658c5046bb2b72
hyperchessbot <73081047+hyperchessbot@users.noreply.github.com>
2021-01-23 15:07:34 +0100

    Update actix_tokio.rs

--- actix_tokio.rs
+++ actix_tokio.rs
@@ -24,3 +24,39 @@ async fn main() -> std::io::Result<()> {
 async fn index() -> &'static str {
     "Hello World!"
 }
+
+/////////////////////////////////////////////////////////
+
+use std::time::Duration;
+
+use actix_web::{get, App, HttpServer};
+
+fn main() -> std::io::Result<()> {
+    let tokio_rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()?;
+
+    tokio_rt.spawn(async {
+        actix_web::rt::time::sleep(Duration::from_secs(1)).await;
+        println!("tokio spawn");
+    });
+
+    let server = actix_web::rt::System::new("abc").block_on(async {
+       let server = HttpServer::new(|| App::new().service(index))
+            .disable_signals()
+            .bind("127.0.0.1:8000")?
+            .run();
+        Ok::<_, std::io::Error>(server)
+    })?;
+
+    tokio_rt.block_on(async move {
+        let _ = tokio::signal::ctrl_c().await;
+        let _ = server.stop(true).await;
+        Ok(())
+    })
+}
+
+#[get("/")]
+async fn index() -> &'static str {
+    "Hello World!"
+}
